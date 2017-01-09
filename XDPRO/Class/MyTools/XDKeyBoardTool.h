10d3a9124e9c2f4e11ac9fcd0c8b544b195af23b
Xiadian <shuaishuaishen1@163.com>
2017-01-09 11:41:45 +0800

    dfdsf

---
+++ XDPRO/Class/MyTools/XDKeyBoardTool.h
@@ -0,0 +1,13 @@
+//
+//  XDKeyBoardTool.h
+//  XDPRO
+//
+//  Created by XiaDian on 2017/1/6.
+//  Copyright © 2017年 vdchina. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XDKeyBoardTool : NSObject
+
+@end
---
+++ XDPRO/Class/MyTools/XDKeyBoardTool.m
@@ -0,0 +1,13 @@
+//
+//  XDKeyBoardTool.m
+//  XDPRO
+//
+//  Created by XiaDian on 2017/1/6.
+//  Copyright © 2017年 vdchina. All rights reserved.
+//
+
+#import "XDKeyBoardTool.h"
+
+@implementation XDKeyBoardTool
+
+@end
